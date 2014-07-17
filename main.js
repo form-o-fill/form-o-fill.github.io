35646cc35489831685a8e99a1f956488c4d2b3b4
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-07-17 17:03:01 +0200

    remove disfunctional 'install now'

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 (function mainScope() {
   "use strict";
-  document.querySelector("#install-inline").addEventListener("click", function() {
-    chrome.webstore.install("https://chrome.google.com/webstore/detail/iebbppibdpjldhohknhgjoapijellonp");
-  }, false);
 })();
