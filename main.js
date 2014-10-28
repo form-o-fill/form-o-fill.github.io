6ffbc62d5ab50e53f6d370fff0053d38555a69da
Frank Schumacher <frank.schumacher.privat@gmail.com>
2014-10-28 21:26:54 +0100

    try 2

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 (function mainScope() {
   "use strict";
+  $("#install-button").on("click", function() {
+    chrome.webstore.install("https://chrome.google.com/webstore/detail/iebbppibdpjldhohknhgjoapijellonp", function() { alert(1); }, function(err) { alert(err); });
+  });
 })();
